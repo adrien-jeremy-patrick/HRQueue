c0cea7ad99818180980ad554b6aaa716b1b4a3d3
jerlogan <jeremy.logan.web@gmail.com>
2019-05-18 17:54:45 -0500

    new mobile features

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -4,6 +4,85 @@
     console.log("ready");
 
 
+    //This is the logic for the mobile navbar-------
+
+    //Detect whether or not user is scrolling up or down.
+    //Icon shows when user is scrolling up.
+
+    let menuIcon = $('.menu-icon');
+
+    let lastScrollTop = 0;
+    $(window).scroll(function(event){
+        let st = $(this).scrollTop();
+        if (st > lastScrollTop){
+            // downscroll code
+
+            $('.menu-icon').fadeOut();
+
+
+        } else {
+            // upscroll code
+
+            $('.menu-icon').fadeIn();
+        }
+        lastScrollTop = st;
+    });
+
+
+    //When menu icon is clicked!
+
+
+    $('.menu-icon').on('click', function(){
+
+        $('.body-div').animate({
+            opacity: .2,
+            'background-color': 'red'
+        }, 300);
+
+        $(this).css({
+            'display': 'none'
+        });
+
+        $('.x-icon').css({
+           'display': 'inline-block'
+        });
+
+
+        $('.nav-div').animate({
+            height: '85vh'
+        }, 700);
+
+        $('.extended-nav').fadeIn();
+
+
+    });
+
+    $('.x-icon').on('click', function(){
+
+        $('.body-div').animate({
+            opacity: 1
+        }, 300);
+
+        $(this).css({
+            'display': 'none'
+        });
+
+        $('.menu-icon').css({
+            'display': 'inline-block'
+        });
+
+
+        $('.nav-div').animate({
+            height: '100px'
+        }, 700);
+
+        $('.extended-nav').fadeOut();
+
+
+
+    });
+
+
 
 
 })();
