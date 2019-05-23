7168706def009a845de4f4c99b0b1b64ab758358
AdrienVL <adrienlhemann@gmail.com>
2019-05-23 10:27:54 -0500

    saving work

--- src/main/resources/static/js/reports.js
+++ src/main/resources/static/js/reports.js
@@ -1,151 +1,34 @@
 $(document).ready(function () {
 
+    google.charts.load('current', {'packages':['bar']});
+    google.charts.setOnLoadCallback(timeMetrics);
 
-    $('#CasesTable').dataTable({
+    function timeMetrics() {
+        var data = new google.visualization.arrayToDataTable([
+            ['Time Metrics', 'Days:Hours:Minutes'],
+            ["Avg. Customer Wait Time per Case", 44],
+            ["Avg. Resolve Time per Case", 44]
+        ]);
 
-        "scrollY": "200px",
-        "scrollCollapse": true,
-        "scrollX": true,
-        "ajax": {
-            "url": "/reports-cases",
-            "dataSrc": function (json) {
-                var return_data = new Array();
-                var date_created_at;
-                var date_case_open;
-                var date_case_closed;
-                var resolve_time;
-                var customer_wait_time;
-                var customer_comment;
-                var writer;
-
-
-                for (var i = 0; i < json.length; i++) {
-
-
-                    if (json[i].case_open === null && json[i].case_closed === null && json[i].customer_comment === "") {
-
-                        customer_wait_time = "Case Not Assigned";
-
-                        resolve_time = "N/A";
-
-                        customer_comment = "No comments provided";
-
-                        writer = "No Representative associated with this case";
-
-                        date_created_at = new Date(json[i].created_at).toString().replace(/GMT.*/g, "");
-
-
-                    } else if (json[i].case_open === null && json[i].case_closed === null && json[i].customer_comment !== "") {
-
-                        customer_wait_time = "Case Not Assigned";
-
-                        resolve_time = "N/A";
-
-                        customer_comment = "No comments provided";
-
-                        writer = "No Representative associated with this case";
-
-                        date_created_at = new Date(json[i].created_at).toString().replace(/GMT.*/g, "");
-
-
-                    } else if (json[i].case_open !== null && json[i].case_closed === null && json[i].customer_comment === "") {
-
-                        resolve_time = "Under Review";
-
-                        date_created_at = json[i].created_at;
-
-                        date_case_open = json[i].case_open;
-
-                        customer_comment = "No comments provided";
-
-                        writer = json[i].writer.username;
-
-                        customer_wait_time = date_case_open - date_created_at;
-
-                        date_created_at = new Date(json[i].created_at).toString().replace(/GMT.*/g, "");
-
-                        customer_wait_time = dhm(customer_wait_time);
-
-
-                    } else if (json[i].case_open !== null && json[i].case_closed === null && json[i].customer_comment !== "") {
-
-                        resolve_time = "Under Review";
-
-                        date_created_at = json[i].created_at;
-
-                        date_case_open = json[i].case_open;
-
-                        customer_comment = json[i].customer_comment;
-
-                        writer = json[i].writer.username;
-
-                        customer_wait_time = date_case_open - date_created_at;
-
-                        date_created_at = new Date(json[i].created_at).toString().replace(/GMT.*/g, "");
-
-                        customer_wait_time = dhm(customer_wait_time);
-                    } else if (json[i].case_open !== null && json[i].case_closed !== null && json[i].customer_comment !== "") {
-
-                        date_created_at = json[i].created_at;
-
-                        customer_comment = json[i].customer_comment;
-
-                        date_case_open = json[i].case_open;
-
-                        date_case_closed = json[i].case_closed;
-
-                        writer = json[i].writer.username;
-
-                        customer_wait_time = date_case_open - date_created_at;
-
-                        resolve_time = date_case_closed - date_case_open;
-
-                        date_created_at = new Date(json[i].created_at).toString().replace(/GMT.*/g, "");
-
-                        customer_wait_time = dhm(customer_wait_time);
-
-                        resolve_time = dhm(resolve_time);
-
-
-                    }
-
-
-                    return_data.push({
-
-                        'case_closed': json[i].case_closed,
-                        'case_open': json[i].case_open,
-                        'writer': writer,
-
-                        'created_at': date_created_at,
-                        'customer_name': json[i].customer_name,
-                        'department': json[i].department.department,
-                        'category': json[i].category.category,
-                        'customer_comment': customer_comment,
-                        'reps-admins_comments' : 'test',
-                        'customer_wait_time': customer_wait_time,
-                        'resolve_time': resolve_time
-
-                    })
+        var options = {
+            title: 'Time Metrics',
+            width: 900,
+            legend: { position: 'none' },
+            chart: { title: 'Time Metrics',
+                subtitle: 'Performances by Time' },
+            bars: 'horizontal', // Required for Material Bar Charts.
+            axes: {
+                x: {
+                    0: { side: 'bottom', label: 'Days:Hours:Minutes'} // Top x-axis.
                 }
-                return return_data;
-            }
+            },
+            bar: { groupWidth: "90%" }
+        };
 
-        },
-        "sAjaxDataProp": "",
-        "order": [[0, "asc"]],
-        "columns": [
-            {"data": "created_at"},
-            {"data": "customer_name"},
-            {"data": "writer"},
-            {"data": "department"},
-            {"data": "category"},
-            {"data": "customer_comment"},
-            {"data": "reps-admins_comments"},
-            {"data": "customer_wait_time"},
-            {"data": "resolve_time"},
+        var chart = new google.charts.Bar(document.getElementById('BarCharts'));
+        chart.draw(data, options);
+    };
 
-        ]
-    });
 
     $('#performanceTable').dataTable({
 
@@ -310,7 +193,7 @@ $(document).ready(function () {
                 }
 
 
-                // var sortingItems = ['All', 'Category', 'Department', 'Representatives'];
+                // var sortingItems = ['All', 'Category', 'Department', 'Representatives, Time'];
 
 
 
@@ -368,6 +251,151 @@ $(document).ready(function () {
 
     });
 
+    $('#CasesTable').dataTable({
+
+        "scrollY": "200px",
+        "scrollCollapse": true,
+        "scrollX": true,
+        "ajax": {
+            "url": "/reports-cases",
+            "dataSrc": function (json) {
+                var return_data = new Array();
+                var date_created_at;
+                var date_case_open;
+                var date_case_closed;
+                var resolve_time;
+                var customer_wait_time;
+                var customer_comment;
+                var writer;
+
+
+                for (var i = 0; i < json.length; i++) {
+
+
+                    if (json[i].case_open === null && json[i].case_closed === null && json[i].customer_comment === "") {
+
+                        customer_wait_time = "Case Not Assigned";
+
+                        resolve_time = "N/A";
+
+                        customer_comment = "No comments provided";
+
+                        writer = "No Representative associated with this case";
+
+                        date_created_at = new Date(json[i].created_at).toString().replace(/GMT.*/g, "");
+
+
+                    } else if (json[i].case_open === null && json[i].case_closed === null && json[i].customer_comment !== "") {
+
+                        customer_wait_time = "Case Not Assigned";
+
+                        resolve_time = "N/A";
+
+                        customer_comment = "No comments provided";
+
+                        writer = "No Representative associated with this case";
+
+                        date_created_at = new Date(json[i].created_at).toString().replace(/GMT.*/g, "");
+
+
+                    } else if (json[i].case_open !== null && json[i].case_closed === null && json[i].customer_comment === "") {
+
+                        resolve_time = "Under Review";
+
+                        date_created_at = json[i].created_at;
+
+                        date_case_open = json[i].case_open;
+
+                        customer_comment = "No comments provided";
+
+                        writer = json[i].writer.username;
+
+                        customer_wait_time = date_case_open - date_created_at;
+
+                        date_created_at = new Date(json[i].created_at).toString().replace(/GMT.*/g, "");
+
+                        customer_wait_time = dhm(customer_wait_time);
+
+
+                    } else if (json[i].case_open !== null && json[i].case_closed === null && json[i].customer_comment !== "") {
+
+                        resolve_time = "Under Review";
+
+                        date_created_at = json[i].created_at;
+
+                        date_case_open = json[i].case_open;
+
+                        customer_comment = json[i].customer_comment;
+
+                        writer = json[i].writer.username;
+
+                        customer_wait_time = date_case_open - date_created_at;
+
+                        date_created_at = new Date(json[i].created_at).toString().replace(/GMT.*/g, "");
+
+                        customer_wait_time = dhm(customer_wait_time);
+                    } else if (json[i].case_open !== null && json[i].case_closed !== null && json[i].customer_comment !== "") {
+
+                        date_created_at = json[i].created_at;
+
+                        customer_comment = json[i].customer_comment;
+
+                        date_case_open = json[i].case_open;
+
+                        date_case_closed = json[i].case_closed;
+
+                        writer = json[i].writer.username;
+
+                        customer_wait_time = date_case_open - date_created_at;
+
+                        resolve_time = date_case_closed - date_case_open;
+
+                        date_created_at = new Date(json[i].created_at).toString().replace(/GMT.*/g, "");
+
+                        customer_wait_time = dhm(customer_wait_time);
+
+                        resolve_time = dhm(resolve_time);
+
+
+                    }
+
+
+                    return_data.push({
+
+                        'case_closed': json[i].case_closed,
+                        'case_open': json[i].case_open,
+                        'writer': writer,
+
+                        'created_at': date_created_at,
+                        'customer_name': json[i].customer_name,
+                        'department': json[i].department.department,
+                        'category': json[i].category.category,
+                        'customer_comment': customer_comment,
+                        'reps-admins_comments' : 'test',
+                        'customer_wait_time': customer_wait_time,
+                        'resolve_time': resolve_time
+
+                    })
+                }
+                return return_data;
+            }
+
+        },
+        "sAjaxDataProp": "",
+        "order": [[0, "asc"]],
+        "columns": [
+            {"data": "created_at"},
+            {"data": "customer_name"},
+            {"data": "writer"},
+            {"data": "department"},
+            {"data": "category"},
+            {"data": "customer_comment"},
+            {"data": "reps-admins_comments"},
+            {"data": "customer_wait_time"},
+            {"data": "resolve_time"},
+
+        ]
+    });
 
 
 
@@ -390,7 +418,6 @@ $(document).ready(function () {
         }
         return [d, pad(h), pad(m)].join(':');
     }
-
 
 
     function today() {
